d8656474abc7c51d9affc817242f0563b8c84c33
Tutku Ince <tutku.ince@outlook.com>
2024-01-04 13:46:25 +0300

    ProportionToNumberOfItems  for BigO notation -> O(n)

---
+++ src/main/java/io/tince/_03bigonotation/ProportionToNumberOfItems.java
@@ -0,0 +1,16 @@
+package io.tince._03bigonotation;
+
+public class ProportionToNumberOfItems {
+    // O(n) -> means in proportion to the number of items.
+
+    // Do not change the main method below
+    public static void main(String[] args) {
+        printItems(10);
+    }
+
+    private static void printItems(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
+    }
+}
