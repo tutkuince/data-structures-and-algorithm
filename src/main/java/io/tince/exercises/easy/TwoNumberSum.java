08dc3e2dc473fcd13bbfbd372658859e8a8d9bb4
Tutku Ince <tutku.ince@outlook.com>
2024-03-12 21:54:27 +0300

    solution 3

--- src/main/java/io/tince/exercises/easy/TwoNumberSum.java
+++ src/main/java/io/tince/exercises/easy/TwoNumberSum.java
@@ -24,8 +24,28 @@ public class TwoNumberSum {
         System.out.println(Arrays.toString(twoNumberSum(array, targetSum)));
     }
 
-    // O(n) time | O(n) space
+    // O(nlog(n)) time | O(1) space
     public static int[] twoNumberSum(int[] array, int targetSum) {
+        // Write your code here.
+        // [ -4, -1, 1, 3, 5, 6, 8, 11 ]
+        Arrays.sort(array);
+        int left = 0;
+        int right = array.length - 1;
+        while (left < right) {
+            int currentSum = array[left] + array[right];
+            if (currentSum == targetSum) {
+                return new int[]{array[left], array[right]};
+            } else if (currentSum < targetSum) {
+                left++;
+            } else {
+                right++;
+            }
+        }
+        return new int[0];
+    }
+
+    // O(n) time | O(n) space
+    /*public static int[] twoNumberSum(int[] array, int targetSum) {
         // Write your code here.
         Set<Integer> numbersSet = new HashSet<>();
         for (int num :array) {
@@ -37,7 +57,7 @@ public class TwoNumberSum {
             }
         }
         return new int[0];
-    }
+    }*/
 
     // O(n^2) time | O(1) space
     /*public static int[] twoNumberSum(int[] array, int targetSum) {
