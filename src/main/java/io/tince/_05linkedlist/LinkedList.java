e6d495d3160dd039fddaf9fe728aa1430b8a494b
Tutku Ince <tutku.ince@outlook.com>
2024-04-20 18:55:17 +0300

    printList

--- src/main/java/io/tince/_05linkedlist/LinkedList.java
+++ src/main/java/io/tince/_05linkedlist/LinkedList.java
@@ -12,6 +12,26 @@ public class LinkedList {
         length = 1;
     }
 
+    public void printList() {
+        Node currentNode = head;
+        while (currentNode != null) {
+            System.out.println(currentNode.value);
+            currentNode = currentNode.next;
+        }
+    }
+
+    public void getHead() {
+        System.out.println("Head: " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
     class Node {
         int value;
         Node next;
