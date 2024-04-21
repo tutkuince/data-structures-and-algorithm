8184d51d88f06a911e231e6df1e00d599e12dfad
Tutku Ince <tutku.ince@outlook.com>
2024-04-21 23:07:04 +0300

    set(index, value)

--- src/main/java/io/tince/_05linkedlist/LinkedList.java
+++ src/main/java/io/tince/_05linkedlist/LinkedList.java
@@ -47,6 +47,15 @@ public class LinkedList {
         return temp;
     }
 
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+        if (temp != null) {
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
+
     public Node removeFirst() {
         if (length == 0) {
             return null;
