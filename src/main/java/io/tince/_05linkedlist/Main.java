8fe83988f99fbce553331b5370d2920e8f399871
Tutku Ince <tutku.ince@outlook.com>
2024-04-21 22:52:11 +0300

    get Test

--- src/main/java/io/tince/_05linkedlist/Main.java
+++ src/main/java/io/tince/_05linkedlist/Main.java
@@ -5,13 +5,14 @@ public class Main {
         LinkedList myLinkedList = new LinkedList(4);
         myLinkedList.append(6);
         myLinkedList.prepend(2);
-        myLinkedList.removeFirst();
-        myLinkedList.removeFirst();
+        /*myLinkedList.removeFirst();
+        myLinkedList.removeFirst();*/
         myLinkedList.getHead();
         myLinkedList.getTail();
         myLinkedList.getLength();
         myLinkedList.printList();
         System.out.println("-------------------");
+        System.out.println(myLinkedList.get(3).value);
         // (2) Items - Returns 2 Node
         // System.out.println(myLinkedList.removeLast().value);
         // (1) Items - Returns 1 Node
