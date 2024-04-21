8943c208399b44f018d1f552f07e4e085092cefd
Tutku Ince <tutku.ince@outlook.com>
2024-04-21 18:23:17 +0300

    prepend()

--- src/main/java/io/tince/_05linkedlist/Main.java
+++ src/main/java/io/tince/_05linkedlist/Main.java
@@ -4,17 +4,17 @@ public class Main {
     public static void main(String[] args) {
         LinkedList myLinkedList = new LinkedList(4);
         myLinkedList.append(6);
-
+        myLinkedList.prepend(2);
         myLinkedList.getHead();
         myLinkedList.getTail();
         myLinkedList.getLength();
         myLinkedList.printList();
         System.out.println("-------------------");
         // (2) Items - Returns 2 Node
-        System.out.println(myLinkedList.removeLast().value);
+        // System.out.println(myLinkedList.removeLast().value);
         // (1) Items - Returns 1 Node
-        System.out.println(myLinkedList.removeLast().value);
+        // System.out.println(myLinkedList.removeLast().value);
         // (0) Items - Returns 1 Node
-        System.out.println(myLinkedList.removeLast());
+        // System.out.println(myLinkedList.removeLast());
     }
 }
