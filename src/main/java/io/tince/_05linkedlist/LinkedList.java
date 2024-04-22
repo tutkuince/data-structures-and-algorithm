31e99f6dccdcbb89f7291271ebc5554970c77620
Tutku Ince <tutku.ince@outlook.com>
2024-04-22 20:41:12 +0300

    remove(index)

--- src/main/java/io/tince/_05linkedlist/LinkedList.java
+++ src/main/java/io/tince/_05linkedlist/LinkedList.java
@@ -76,6 +76,24 @@ public class LinkedList {
         return true;
     }
 
+    public Node remove(int index) {
+        if (index < 0 || index > length) {
+            return null;
+        }
+        if (index == 0) {
+            return removeFirst();
+        }
+        if (index == length - 1) {
+            return removeLast();
+        }
+        Node prev = get(index - 1);
+        Node temp = prev.next;
+        prev.next = temp.next;
+        temp.next = null;
+        length--;
+        return temp;
+    }
+
     public Node removeFirst() {
         if (length == 0) {
             return null;
