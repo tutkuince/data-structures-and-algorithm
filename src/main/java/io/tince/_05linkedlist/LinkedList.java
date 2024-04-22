9cf0dae3226577e7d64ce3cea3d6e7e6a5495e94
Tutku Ince <tutku.ince@outlook.com>
2024-04-22 20:52:36 +0300

    reverse()

--- src/main/java/io/tince/_05linkedlist/LinkedList.java
+++ src/main/java/io/tince/_05linkedlist/LinkedList.java
@@ -128,6 +128,20 @@ public class LinkedList {
         return temp;
     }
 
+    public void reverse() {
+        Node temp = head;
+        head = tail;
+        tail = temp;
+        Node after = temp.next;
+        Node before = null;
+        for (int i = 0; i < length; i++) {
+            after = temp.next;
+            temp.next = before;
+            before = temp;
+            temp = after;
+        }
+    }
+
     public void printList() {
         Node currentNode = head;
         while (currentNode != null) {
