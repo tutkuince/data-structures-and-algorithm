1d4397acd1781cacb9ff8f83e9632e9428cb09d5
Tutku Ince <tutku.ince@outlook.com>
2024-04-24 15:27:28 +0300

    solution 1

---
+++ src/main/java/io/tince/_05linkedlist/interview/questions/FindKthNodeFromEnd.java
@@ -0,0 +1,113 @@
+package io.tince._05linkedlist.interview.questions;
+
+/**
+ * Find Kth Node From End
+ * <p>
+ * Implement a method called "findKthFromEnd" that returns the k-th node from the end of the list
+ * <p>
+ * If the list has fewer than k nodes, the method should return "null".
+ * <p>
+ * Note: This implementation of the LinkedList class does not have the "length" attribute.
+ * <p>
+ * Method Signature: public Node findKthFromEnd(int k)
+ */
+public class FindKthNodeFromEnd {
+    private Node head;
+    private Node tail;
+
+    public static void main(String[] args) {
+        FindKthNodeFromEnd myList = new FindKthNodeFromEnd(1);
+        myList.append(2);
+        myList.append(3);
+        myList.append(4);
+        myList.append(5);
+
+        Node result = myList.findKthFromEnd(2); // Output: Node with value 4
+        System.out.println(result.value);
+    }
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public FindKthNodeFromEnd(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (head == null) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("\nLinked List:");
+        if (head == null) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    // WRITE FINDKTHFROMEND METHOD HERE //
+    //                                  //
+    //                                  //
+    //                                  //
+    //                                  //
+    //////////////////////////////////////
+    public Node findKthFromEnd(int k) {
+        int counter = 1;
+        Node temp = head;
+        while (temp != null && temp.next != null) {
+            temp = temp.next;
+            counter++;
+        }
+        if (k > counter) return null;
+        int index = counter - k;
+        temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+}
