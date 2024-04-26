f0e3c43b3ad67312fc8c561539cdca99a0c95b48
Tutku Ince <tutku.ince@outlook.com>
2024-04-26 13:48:06 +0300

    Doubly Linked List (consturctor)

---
+++ src/main/java/io/tince/_06doublylinkedlist/DoublyLinkedList.java
@@ -0,0 +1,44 @@
+package io.tince._06doublylinkedlist;
+
+public class DoublyLinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public DoublyLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        System.out.println("Head: " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+}
