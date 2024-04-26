f8a8b079c7a8b94200a3acaea36493008c8aeaef
Tutku Ince <tutku.ince@outlook.com>
2024-04-26 14:07:57 +0300

    test removeLast

--- src/main/java/io/tince/_06doublylinkedlist/Main.java
+++ src/main/java/io/tince/_06doublylinkedlist/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         DoublyLinkedList doublyLinkedList = new DoublyLinkedList(7);
         doublyLinkedList.append(2);
+        doublyLinkedList.append(1);
+        doublyLinkedList.removeLast();
 
         doublyLinkedList.getHead();
         doublyLinkedList.getTail();
