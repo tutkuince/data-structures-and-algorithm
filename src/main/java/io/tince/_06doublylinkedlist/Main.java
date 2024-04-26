8b8c387ca7330fc73613ddb0e72b55e659ac64fa
Tutku Ince <tutku.ince@outlook.com>
2024-04-26 13:57:13 +0300

    testAppend(value)

--- src/main/java/io/tince/_06doublylinkedlist/Main.java
+++ src/main/java/io/tince/_06doublylinkedlist/Main.java
@@ -3,6 +3,7 @@ package io.tince._06doublylinkedlist;
 public class Main {
     public static void main(String[] args) {
         DoublyLinkedList doublyLinkedList = new DoublyLinkedList(7);
+        doublyLinkedList.append(2);
 
         doublyLinkedList.getHead();
         doublyLinkedList.getTail();
