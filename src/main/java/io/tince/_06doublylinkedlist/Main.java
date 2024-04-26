59109e60712996d6b0ce0575c67a839651647933
Tutku Ince <tutku.ince@outlook.com>
2024-04-26 13:49:19 +0300

    Test Constructor

---
+++ src/main/java/io/tince/_06doublylinkedlist/Main.java
@@ -0,0 +1,13 @@
+package io.tince._06doublylinkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+        DoublyLinkedList doublyLinkedList = new DoublyLinkedList(7);
+
+        doublyLinkedList.getHead();
+        doublyLinkedList.getTail();
+        doublyLinkedList.getLength();
+
+        doublyLinkedList.printList();
+    }
+}
