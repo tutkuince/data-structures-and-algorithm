6f088996165a280d8e2efe3a0e760cd0225b5bc8
Tutku Ince <tutku.ince@outlook.com>
2024-04-27 15:12:36 +0300

    get(index) test

--- src/main/java/io/tince/_06doublylinkedlist/Main.java
+++ src/main/java/io/tince/_06doublylinkedlist/Main.java
@@ -5,13 +5,17 @@ public class Main {
         DoublyLinkedList doublyLinkedList = new DoublyLinkedList(7);
         doublyLinkedList.append(2);
         doublyLinkedList.append(1);
-        System.out.println("Removed Last Node Value is: " + doublyLinkedList.removeLast().value);
+        // System.out.println("Removed Last Node Value is: " + doublyLinkedList.removeLast().value);
         doublyLinkedList.prepend(9);
-        System.out.println("Removed First Node Value is: " + doublyLinkedList.removeFirst().value);
+        // System.out.println("Removed First Node Value is: " + doublyLinkedList.removeFirst().value);
+
+
+//        doublyLinkedList.append(3);
+//        doublyLinkedList.append(5);
+        doublyLinkedList.printList();
         doublyLinkedList.getHead();
         doublyLinkedList.getTail();
         doublyLinkedList.getLength();
-
-        doublyLinkedList.printList();
+        System.out.println(doublyLinkedList.get(3).value);
     }
 }
