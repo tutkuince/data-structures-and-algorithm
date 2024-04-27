09dc63a69af349786e5bd18c9b79123c7a26ce3d
Tutku Ince <tutku.ince@outlook.com>
2024-04-27 15:48:58 +0300

    insert(index, value)

--- src/main/java/io/tince/_06doublylinkedlist/DoublyLinkedList.java
+++ src/main/java/io/tince/_06doublylinkedlist/DoublyLinkedList.java
@@ -37,6 +37,29 @@ public class DoublyLinkedList {
         length++;
     }
 
+    public boolean insert(int index, int value) {
+        if (index < 0 || index > length) {
+            return false;
+        }
+        if (index == 0) {
+            prepend(value);
+            return true;
+        }
+        if (index == length) {
+            append(value);
+            return true;
+        }
+        Node newNode = new Node(value);
+        Node before = get(index - 1);
+        Node after = before.next;
+        newNode.prev = before;
+        newNode.next = after;
+        before.next = newNode;
+        after.prev = newNode;
+        length++;
+        return true;
+    }
+
     public Node get(int index) {
         if (index < 0 || index >= length) {
             return null;
