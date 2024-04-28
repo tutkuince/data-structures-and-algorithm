c97339772beebd0e2478b702d8baf6bbcb60d511
Tutku Ince <tutku.ince@outlook.com>
2024-04-28 18:06:59 +0300

    get

--- src/main/java/io/tince/_06doublylinkedlist/exercise/DoublyLinkedList.java
+++ src/main/java/io/tince/_06doublylinkedlist/exercise/DoublyLinkedList.java
@@ -44,6 +44,24 @@ public class DoublyLinkedList {
         length++;
     }
 
+    public Node get(int index) {
+        if (index < 0 || index >= length) {
+            return null;
+        }
+        Node temp = head;
+        if (index < length / 2) {
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+        } else {
+            temp = tail;
+            for (int i = length - 1; i > index; i--) {
+                temp = temp.prev;
+            }
+        }
+        return temp;
+    }
+
     public Node removeFirst() {
         if (length == 0) {
             return null;
