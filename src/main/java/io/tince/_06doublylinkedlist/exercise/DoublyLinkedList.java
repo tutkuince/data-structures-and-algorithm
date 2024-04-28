3de5db62d9dab0b069b5a94c5ab7a18776ec8017
Tutku Ince <tutku.ince@outlook.com>
2024-04-28 16:57:47 +0300

    Constructor

---
+++ src/main/java/io/tince/_06doublylinkedlist/exercise/DoublyLinkedList.java
@@ -0,0 +1,66 @@
+package io.tince._06doublylinkedlist.exercise;
+
+public class DoublyLinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+    // CREATE CLASS VARIABLES, NODE CLASS, AND CONSTRUCTOR HERE //
+    //                                                          //
+    //                                                          //
+    //                                                          //
+    //                                                          //
+    //////////////////////////////////////////////////////////////
+    public DoublyLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nDoubly Linked List:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+    class Node {
+        Node next;
+        Node prev;
+        int value;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+}
