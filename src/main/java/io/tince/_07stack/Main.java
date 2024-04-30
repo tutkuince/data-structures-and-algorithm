a89668851acdc6cb7668d020b7bc4fa1851683fd
Tutku Ince <tutku.ince@outlook.com>
2024-04-30 22:04:27 +0300

    push test

--- src/main/java/io/tince/_07stack/Main.java
+++ src/main/java/io/tince/_07stack/Main.java
@@ -3,7 +3,7 @@ package io.tince._07stack;
 public class Main {
     public static void main(String[] args) {
         Stack myStack = new Stack(4);
-
+        myStack.push(2);
         myStack.getTop();
         myStack.getHeight();
         myStack.printStack();
