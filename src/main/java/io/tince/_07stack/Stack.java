e100dd8abf1f914e704795986b537537d7b23c0e
Tutku Ince <tutku.ince@outlook.com>
2024-04-30 22:04:21 +0300

    push

--- src/main/java/io/tince/_07stack/Stack.java
+++ src/main/java/io/tince/_07stack/Stack.java
@@ -9,6 +9,17 @@ public class Stack {
         height = 1;
     }
 
+    public void push(int value) {
+        Node newNode = new Node(value);
+        if (height == 0) {
+            top = newNode;
+        } else {
+            newNode.next = top;
+            top = newNode;
+        }
+        height++;
+    }
+
     public void printStack() {
         Node temp = top;
         while (temp != null) {
