c3c620f917a4f8ad507502c9644f5ff1f30d8c2a
Tutku Ince <tutku.ince@outlook.com>
2024-04-30 21:32:21 +0300

    print

--- src/main/java/io/tince/_07stack/Stack.java
+++ src/main/java/io/tince/_07stack/Stack.java
@@ -9,6 +9,22 @@ public class Stack {
         height = 1;
     }
 
+    public void printStack() {
+        Node temp = top;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getTop() {
+        System.out.println("Top: " + top.value);
+    }
+
+    public void getHeight() {
+        System.out.println("Height: " + height);
+    }
+
     class Node {
         int value;
         Node next;
