2dbd11950e97e9e291771d7f30ccabdfec9b8bfb
Tutku Ince <tutku.ince@outlook.com>
2024-05-01 22:38:27 +0300

    dequeue() test

--- src/main/java/io/tince/_08queue/Main.java
+++ src/main/java/io/tince/_08queue/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Queue myQueue = new Queue(7);
         myQueue.enqueue(11);
-
+        myQueue.enqueue(15);
+        myQueue.dequeue();
         myQueue.getFirst();
         myQueue.getLast();
         myQueue.getLength();
