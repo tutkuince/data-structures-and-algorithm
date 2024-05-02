dfcc6d50ec00e249e4f5fa4b6cd5f5f9b44abb05
Tutku Ince <tutku.ince@outlook.com>
2024-05-02 13:49:16 +0300

    pop

--- src/main/java/io/tince/_07stack/exercise/Stack.java
+++ src/main/java/io/tince/_07stack/exercise/Stack.java
@@ -77,4 +77,19 @@ public class Stack {
         }
         height++;
     }
+
+    /// WRITE POP METHOD HERE ///
+    //                         //
+    //                         //
+    //                         //
+    //                         //
+    /////////////////////////////
+    public Node pop() {
+        if (height == 0) return null;
+        Node temp = top;
+        top = top.next;
+        temp.next = null;
+        height--;
+        return temp;
+    }
 }
