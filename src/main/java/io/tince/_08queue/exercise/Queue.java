b33f4c443d24a6f982f6942363cf325594a4e483
Tutku Ince <tutku.ince@outlook.com>
2024-05-02 13:53:32 +0300

    constructor

---
+++ src/main/java/io/tince/_08queue/exercise/Queue.java
@@ -0,0 +1,73 @@
+package io.tince._08queue.exercise;
+
+public class Queue {
+    // CREATE CLASS VARIABLES, NODE CLASS, AND CONSTRUCTOR HERE //
+    //                                                          //
+    //                                                          //
+    //                                                          //
+    //                                                          //
+    //////////////////////////////////////////////////////////////
+    private Node first;
+    private Node last;
+    private int length;
+
+    public Queue(int value) {
+        Node newNode = new Node(value);
+        first = newNode;
+        last = newNode;
+        length = 1;
+    }
+
+    class Node {
+        Node next;
+        int value;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+
+    public Node getFirst() {
+        return first;
+    }
+
+    public Node getLast() {
+        return last;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = first;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (length == 0) {
+            System.out.println("First: null");
+            System.out.println("Last: null");
+        } else {
+            System.out.println("First: " + first.value);
+            System.out.println("Last: " + last.value);
+        }
+        System.out.println("Length:" + length);
+        System.out.println("\nQueue:");
+        if (length == 0) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+
+    public void makeEmpty() {
+        first = null;
+        last = null;
+        length = 0;
+    }
+}
