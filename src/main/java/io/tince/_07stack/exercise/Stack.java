8c92e252c0dac3bbc0a5a1ada0d023f3d7354777
Tutku Ince <tutku.ince@outlook.com>
2024-05-02 13:45:03 +0300

    push

---
+++ src/main/java/io/tince/_07stack/exercise/Stack.java
@@ -0,0 +1,80 @@
+package io.tince._07stack.exercise;
+
+public class Stack {
+    // CREATE CLASS VARIABLES, NODE CLASS, AND CONSTRUCTOR HERE//
+    //                                                         //
+    //                                                         //
+    //                                                         //
+    //                                                         //
+    /////////////////////////////////////////////////////////////
+    private Node top;
+    private int height;
+
+    class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Stack(int value) {
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+
+    public Node getTop() {
+        return top;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void printStack() {
+        Node temp = top;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (height == 0) {
+            System.out.println("Top: null");
+        } else {
+            System.out.println("Top: " + top.value);
+        }
+        System.out.println("Height:" + height);
+        System.out.println("\nStack:");
+        if (height == 0) {
+            System.out.println("empty");
+        } else {
+            printStack();
+        }
+    }
+
+    public void makeEmpty() {
+        top = null;
+        height = 0;
+    }
+
+    /// WRITE PUSH METHOD HERE ///
+    //                          //
+    //                          //
+    //                          //
+    //                          //
+    //////////////////////////////
+    public void push(int value) {
+        Node newNode = new Node(value);
+        if (height == 0) {
+            top = newNode;
+        } else {
+            newNode.next = top;
+            top = newNode;
+        }
+        height++;
+    }
+}
