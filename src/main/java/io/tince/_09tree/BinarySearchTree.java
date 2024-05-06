e91f26f3c9eef7a8acb125d69d2a85c2a818f1c7
Tutku Ince <tutku.ince@outlook.com>
2024-05-06 16:43:58 +0300

    insert test

--- src/main/java/io/tince/_09tree/BinarySearchTree.java
+++ src/main/java/io/tince/_09tree/BinarySearchTree.java
@@ -1,10 +1,37 @@
 package io.tince._09tree;
 
+import java.util.Objects;
+
 public class BinarySearchTree {
     private Node root;
 
     public Node getRoot() {
         return root;
+    }
+
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        if (Objects.isNull(root)) {
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while (true) {
+            if (newNode.value == temp.value) return false;
+            if (newNode.value < temp.value) {
+                if (Objects.isNull(temp.left)) {
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            } else {
+                if (Objects.isNull(temp.right)) {
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
     }
 
     class Node {
