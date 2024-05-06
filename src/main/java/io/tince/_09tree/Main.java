e0c07cc9b8d642218ec30cb9c4c10b25b3120dc8
Tutku Ince <tutku.ince@outlook.com>
2024-05-06 16:43:52 +0300

    insert

--- src/main/java/io/tince/_09tree/Main.java
+++ src/main/java/io/tince/_09tree/Main.java
@@ -3,7 +3,15 @@ package io.tince._09tree;
 public class Main {
     public static void main(String[] args) {
         BinarySearchTree myBST = new BinarySearchTree();
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(52);
+        myBST.insert(82);
 
-        System.out.println("root: " + myBST.getRoot());
+        myBST.insert(27);
+        System.out.println("root: " + myBST.getRoot().value);
+        System.out.println(myBST.getRoot().left.right.value);
     }
 }
