6fda4b523bec5484574e54ac75dd558eada7cbbd
Tutku Ince <tutku.ince@outlook.com>
2024-05-07 16:21:29 +0300

    printTable

--- src/main/java/io/tince/_10hashtables/HashTable.java
+++ src/main/java/io/tince/_10hashtables/HashTable.java
@@ -8,6 +8,17 @@ public class HashTable {
         dataMap = new Node[size];
     }
 
+    public void printTable() {
+        for (int i = 0; i < dataMap.length; i++) {
+            System.out.println(i + ":");
+            Node temp = dataMap[i];
+            while (temp != null) {
+                System.out.println("{" + temp.key + "= " + temp.value + "}");
+                temp = temp.next;
+            }
+        }
+    }
+
     class Node {
         String key;
         int value;
