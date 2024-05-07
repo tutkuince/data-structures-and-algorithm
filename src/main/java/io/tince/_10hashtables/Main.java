f9bdc8d25bb34167051e8e99a87493a0ee693cd2
Tutku Ince <tutku.ince@outlook.com>
2024-05-07 16:23:15 +0300

    test constructor for hash table

---
+++ src/main/java/io/tince/_10hashtables/Main.java
@@ -0,0 +1,8 @@
+package io.tince._10hashtables;
+
+public class Main {
+    public static void main(String[] args) {
+        HashTable myHashTable = new HashTable();
+        myHashTable.printTable();
+    }
+}
