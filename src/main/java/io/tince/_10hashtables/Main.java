ad2f307eb9b2bd13a0e4ea29432a6999e2f5d0c6
Tutku Ince <tutku.ince@outlook.com>
2024-05-08 18:32:09 +0300

    test set

--- src/main/java/io/tince/_10hashtables/Main.java
+++ src/main/java/io/tince/_10hashtables/Main.java
@@ -3,6 +3,12 @@ package io.tince._10hashtables;
 public class Main {
     public static void main(String[] args) {
         HashTable myHashTable = new HashTable();
+        myHashTable.set("nails", 100);
+        myHashTable.set("tile", 50);
+        myHashTable.set("lumber", 80);
+        myHashTable.set("bolts", 200);
+        myHashTable.set("screws", 140);
+
         myHashTable.printTable();
     }
 }
