375f2091cf9231e6d8b1de0b27affa373d81d8d0
Tutku Ince <tutku.ince@outlook.com>
2024-05-09 13:56:44 +0300

    constructor

---
+++ src/main/java/io/tince/_10hashtables/exercise/HashTable.java
@@ -0,0 +1,44 @@
+package io.tince._10hashtables.exercise;
+
+public class HashTable {
+    // CREATE CLASS VARIABLES, NODE CLASS, AND CONSTRUCTOR HERE//
+    //                                                         //
+    //                                                         //
+    //                                                         //
+    //                                                         //
+    /////////////////////////////////////////////////////////////
+    private int size = 7;
+    private Node[] dataMap;
+
+    class Node {
+        String key;
+        int value;
+        Node next;
+
+        Node(String key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public HashTable() {
+        dataMap = new Node[size];
+    }
+
+    public Node[] getDataMap() {
+        return dataMap;
+    }
+
+    public void printTable() {
+        for(int i = 0; i < dataMap.length; i++) {
+            System.out.println(i + ":");
+            if(dataMap[i] != null) {
+                Node temp = dataMap[i];
+                while (temp != null) {
+                    System.out.println("   {" + temp.key + ", " + temp.value + "}");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
