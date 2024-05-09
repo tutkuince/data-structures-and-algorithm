470a313f8b349d0cb0e1d774e06257edaa806445
Tutku Ince <tutku.ince@outlook.com>
2024-05-09 15:59:58 +0300

    solution 2

--- src/main/java/io/tince/_10hashtables/interview/questions/FindDuplicates.java
+++ src/main/java/io/tince/_10hashtables/interview/questions/FindDuplicates.java
@@ -32,6 +32,7 @@ public class FindDuplicates {
 
     }
 
+    // Solution 1
     private static List<Integer> findDuplicates(int[] nums) {
         Map<Integer, Boolean> resultMap = new HashMap<>();
         List<Integer> result = new ArrayList<>();
@@ -44,5 +45,20 @@ public class FindDuplicates {
             }
         }
         return result;
+    }
+
+    // Solution 2
+    public static List<Integer> findDuplicates2(int[] nums) {
+        Map<Integer, Integer> numCounts = new HashMap<>();
+        for (int num : nums) {
+            numCounts.put(num, numCounts.getOrDefault(num, 0) + 1);
+        }
+        List<Integer> duplicates = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : numCounts.entrySet()) {
+            if (entry.getValue() > 1) {
+                duplicates.add(entry.getKey());
+            }
+        }
+        return duplicates;
     }
 }
