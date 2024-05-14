ac9c7f729e30756a7ed2fb2de3c9b9f92fb6bfe0
Tutku Ince <tutku.ince@outlook.com>
2024-05-14 19:06:22 +0300

    test add edge

---
+++ src/main/java/io/tince/_11graphs/Main.java
@@ -0,0 +1,9 @@
+package io.tince._11graphs;
+
+public class Main {
+    public static void main(String[] args) {
+        Graph myGraph = new Graph();
+        myGraph.addVertex("A");
+        myGraph.printGraph();
+    }
+}
