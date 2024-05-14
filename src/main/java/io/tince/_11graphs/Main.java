2147624334cf1de9b6ce81dcb17a6dba82b8d9aa
Tutku Ince <tutku.ince@outlook.com>
2024-05-14 23:40:05 +0300

    removeEdge(vertex1, vertex2)

--- src/main/java/io/tince/_11graphs/Main.java
+++ src/main/java/io/tince/_11graphs/Main.java
@@ -5,7 +5,12 @@ public class Main {
         Graph myGraph = new Graph();
         myGraph.addVertex("A");
         myGraph.addVertex("B");
+        myGraph.addVertex("C");
         myGraph.addEdge("A", "B");
+        myGraph.addEdge("A", "C");
+        myGraph.addEdge("B", "C");
+        myGraph.printGraph();
+        myGraph.removeEdge("A", "B");
         myGraph.printGraph();
     }
 }
