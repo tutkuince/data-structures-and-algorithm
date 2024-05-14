ec8ec4596c1bebd3f00575afdfdff12502c3ec3c
Tutku Ince <tutku.ince@outlook.com>
2024-05-14 17:38:04 +0300

    Find Pairs

--- src/main/java/io/tince/_10hashtables/hashset/interview/questions/FindPairs.java
+++ src/main/java/io/tince/_10hashtables/hashset/interview/questions/FindPairs.java
@@ -4,16 +4,15 @@ import java.util.*;
 
 /**
  * Find Pairs
- *
+ * <p>
  * Given two integer arrays "arr1" and "arr2", and an integer "target",
  * write a method named "findPairs" that finds all pairs of integers such that one integer from "arr1", the other is from "arr2",
  * and their sum equals the target value.
- *
+ * <p>
  * The method should return a list of integer arrays, each containing a pair of integers.
- *
+ * <p>
  * Method Signature: public static List<int[]> findPairs(int[] arr1, int[] arr2, int target)
- *
- * */
+ */
 public class FindPairs {
     // WRITE FIND PAIRS METHOD HERE //
     //                             //
@@ -22,7 +21,19 @@ public class FindPairs {
     //                             //
     /////////////////////////////////
     public static List<int[]> findPairs(int[] arr1, int[] arr2, int target) {
-        return null;
+        List<int[]> pairList = new ArrayList<>();
+        Set<Integer> integerSet = new HashSet<>();
+        for (int i : arr1) {
+            integerSet.add(i);
+        }
+
+        for (int i : arr2) {
+            int requiredValue = target - i;
+            if (integerSet.contains(requiredValue)) {
+                pairList.add(new int[]{requiredValue, i});
+            }
+        }
+        return pairList;
     }
 
 
