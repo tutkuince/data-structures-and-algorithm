c155cc5f8db056795a831ecb57becc16ce46a002
Tutku Ince <tutku.ince@outlook.com>
2024-05-14 23:28:09 +0300

    addEdge(vertex1, vertex2)

--- src/main/java/io/tince/_11graphs/Main.java
+++ src/main/java/io/tince/_11graphs/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Graph myGraph = new Graph();
         myGraph.addVertex("A");
+        myGraph.addVertex("B");
+        myGraph.addEdge("A", "B");
         myGraph.printGraph();
     }
 }
