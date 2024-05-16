da67b45ac55b21f42cb6903decb10d2e1c1aac07
Tutku Ince <tutku.ince@outlook.com>
2024-05-16 15:35:17 +0300

    helper methods

--- src/main/java/io/tince/_12heap/Heap.java
+++ src/main/java/io/tince/_12heap/Heap.java
@@ -13,4 +13,22 @@ public class Heap {
     public List<Integer> getHeap() {
         return heap;
     }
+
+    private void swap(int index1, int index2) {
+        int temp = heap.get(index1);
+        heap.set(index1, heap.get(index2));
+        heap.set(index2, heap.get(temp));
+    }
+
+    private int parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    private int leftChild(int index) {
+        return 2 * index + 1;
+    }
+
+    private int rightChild(int index) {
+        return 2 * index + 2;
+    }
 }
