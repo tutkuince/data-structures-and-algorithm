3de0ec93ff5194184032f5f89bfd12201122e33c
Tutku Ince <tutku.ince@outlook.com>
2024-05-16 18:26:58 +0300

    insert(value)

--- src/main/java/io/tince/_12heap/Heap.java
+++ src/main/java/io/tince/_12heap/Heap.java
@@ -14,10 +14,20 @@ public class Heap {
         return heap;
     }
 
+    public void insert(int value) {
+        heap.add(value);
+        int current = heap.size() - 1;
+
+        while (current > 0 && heap.get(current) > heap.get(parent(current))) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+    }
+
     private void swap(int index1, int index2) {
         int temp = heap.get(index1);
         heap.set(index1, heap.get(index2));
-        heap.set(index2, heap.get(temp));
+        heap.set(index2, temp);
     }
 
     private int parent(int index) {
