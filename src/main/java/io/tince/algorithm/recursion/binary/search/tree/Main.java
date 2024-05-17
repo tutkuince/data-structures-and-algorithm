795c50f207b8afb2c73db6f3c7236fa9c7569ce7
Tutku Ince <tutku.ince@outlook.com>
2024-05-17 17:09:52 +0300

    insert test

--- src/main/java/io/tince/algorithm/recursion/binary/search/tree/Main.java
+++ src/main/java/io/tince/algorithm/recursion/binary/search/tree/Main.java
@@ -3,7 +3,7 @@ package io.tince.algorithm.recursion.binary.search.tree;
 public class Main {
     public static void main(String[] args) {
         BinarySearchTree myBST = new BinarySearchTree();
-        myBST.insert(47);
+        /*myBST.insert(47);
         myBST.insert(21);
         myBST.insert(76);
         myBST.insert(18);
@@ -15,6 +15,13 @@ public class Main {
         System.out.println(myBST.rContains(27));
 
         System.out.println("\nBST Contains 17: ");
-        System.out.println(myBST.rContains(17));
+        System.out.println(myBST.rContains(17));*/
+        myBST.rInsert(2);
+        myBST.rInsert(1);
+        myBST.rInsert(3);
+
+        System.out.println("\nRoot: " + myBST.getRoot().value);
+        System.out.println("\nRoot->Left: " + myBST.getRoot().left.value);
+        System.out.println("\nRoot->Right: " + myBST.getRoot().right.value);
     }
 }
