2019e9b4d7c4074b7a4a7b7be99face04991effd
Tutku Ince <tutku.ince@outlook.com>
2024-05-17 16:06:11 +0300

    contains test

---
+++ src/main/java/io/tince/algorithm/recursion/binary/search/tree/Main.java
@@ -0,0 +1,20 @@
+package io.tince.algorithm.recursion.binary.search.tree;
+
+public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree myBST = new BinarySearchTree();
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(27);
+        myBST.insert(52);
+        myBST.insert(82);
+
+        System.out.println("\nBST Contains 27: ");
+        System.out.println(myBST.rContains(27));
+
+        System.out.println("\nBST Contains 17: ");
+        System.out.println(myBST.rContains(17));
+    }
+}
