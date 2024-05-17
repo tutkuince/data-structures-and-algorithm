317baf82c290e7caa54c14e7111f2209572e4ed4
Tutku Ince <tutku.ince@outlook.com>
2024-05-17 13:46:49 +0300

    insert

---
+++ src/main/java/io/tince/_12heap/exercise/Heap.java
@@ -0,0 +1,49 @@
+package io.tince._12heap.exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Heap {
+    private List<Integer> heap;
+
+    public Heap() {
+        this.heap = new ArrayList<>();
+    }
+
+    public List<Integer> getHeap() {
+        return new ArrayList<>(heap);
+    }
+
+    private int leftChild(int index) {
+        return 2 * index + 1;
+    }
+
+    private int rightChild(int index) {
+        return 2 * index + 2;
+    }
+
+    private int parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    private void swap(int index1, int index2) {
+        int temp = heap.get(index1);
+        heap.set(index1, heap.get(index2));
+        heap.set(index2, temp);
+    }
+
+    // WRITE THE INSERT METHOD HERE //
+    //                              //
+    //                              //
+    //                              //
+    //                              //
+    //////////////////////////////////
+    public void insert(int value) {
+        heap.add(value);
+        int current = heap.size() - 1;
+        while (current > 0 && heap.get(current) > heap.get(parent(current))) {
+            swap(current, parent(current));
+            current = parent(current);
+        }
+    }
+}
