cd0083394f90dabb96cdb9e3ef5792c2800c509d
Tutku Ince <tutku.ince@outlook.com>
2024-05-20 15:45:59 +0300

    contains

---
+++ src/main/java/io/tince/algorithm/recursion/binary/search/tree/exercise/BinarySearchTree.java
@@ -0,0 +1,80 @@
+package io.tince.algorithm.recursion.binary.search.tree.exercise;
+
+import java.util.Objects;
+
+public class BinarySearchTree {
+    private Node root;
+
+    class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while (true) {
+            if (newNode.value == temp.value) return false;
+            if (newNode.value < temp.value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+    }
+
+    public boolean contains(int value) {
+        if (root == null) return false;
+        Node temp = root;
+        while (temp != null) {
+            if (value < temp.value) {
+                temp = temp.left;
+            } else if (value > temp.value) {
+                temp = temp.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // WRITE THE RECURSIVE CONTAINS METHOD HERE //
+    //                                          //
+    //                                          //
+    //                                          //
+    //                                          //
+    //////////////////////////////////////////////
+    private boolean rContains(Node root, int value) {
+        if (Objects.isNull(root)) return false;
+        if (value > root.value) {
+            return rContains(root.right, value);
+        } else if (value < root.value) {
+            return rContains(root.left, value);
+        } else {
+            return true;
+        }
+    }
+
+    public boolean rContains(int value) { return rContains(root, value); }
+}
