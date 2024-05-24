4cfe1a8cd2be55a7c1d3b8653c9d7faf077a8f20
Tutku Ince <tutku.ince@outlook.com>
2024-05-24 15:04:56 +0300

    Bubble Sort

---
+++ src/main/java/io/tince/algorithm/sort/basic/bubble/exercise/BubbleSort.java
@@ -0,0 +1,40 @@
+package io.tince.algorithm.sort.basic.bubble.exercise;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    // WRITE BUBBLE SORT METHOD HERE//
+    //                              //
+    //                              //
+    //                              //
+    //                              //
+    //////////////////////////////////
+    public static void bubbleSort(int[] myArray) {
+        for (int i = myArray.length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (myArray[j] > myArray[j + 1]) {
+                    int temp = myArray[j];
+                    myArray[j] = myArray[j + 1];
+                    myArray[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] myArray = {4, 2, 6, 5, 1, 3};
+
+        bubbleSort(myArray);
+
+        System.out.println(Arrays.toString(myArray));
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            [1, 2, 3, 4, 5, 6]
+
+         */
+
+    }
+}
