8d34b171906ac115fd35f9d871f0bcf9da63a7f4
Tutku Ince <tutku.ince@outlook.com>
2024-05-25 18:00:05 +0300

    test merge

--- src/main/java/io/tince/algorithm/sort/merge/MergeSort.java
+++ src/main/java/io/tince/algorithm/sort/merge/MergeSort.java
@@ -3,7 +3,12 @@ package io.tince.algorithm.sort.merge;
 import java.util.Arrays;
 
 public class MergeSort {
-    
+    public static void main(String[] args) {
+        int[] array1 = {1, 3, 7, 8};
+        int[] array2 = {2, 4, 5, 6};
+
+        System.out.println(Arrays.toString(merge(array1, array2)));
+    }
 
     // [4, 6, 9] -- [2, 5, 7]
     private static int[] merge(int[] array1, int[] array2) {
