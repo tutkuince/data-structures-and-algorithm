98391f63201647b2826dd49caec784b9bd0f2fd2
Tutku Ince <tutku.ince@outlook.com>
2024-05-27 18:39:23 +0300

    Test pivot

--- src/main/java/io/tince/algorithm/sort/quick/QuickSort.java
+++ src/main/java/io/tince/algorithm/sort/quick/QuickSort.java
@@ -1,13 +1,23 @@
 package io.tince.algorithm.sort.quick;
 
+import java.util.Arrays;
+
 public class QuickSort {
+
+    public static void main(String[] args) {
+        int[] myArray = {4, 6, 1, 7, 3, 2, 5};
+        int returnedIndex = pivot(myArray, 0, 6);
+        System.out.println("Returned Index: " + returnedIndex);
+        System.out.println(Arrays.toString(myArray));
+    }
+
     private static void swap(int[] array, int firstIndex, int secondIndex) {
         int temp = array[firstIndex];
         array[firstIndex] = array[secondIndex];
         array[secondIndex] = temp;
     }
 
-    private int pivot(int[] array, int pivotIndex, int endIndex) {
+    private static int pivot(int[] array, int pivotIndex, int endIndex) {
         int swapIndex = pivotIndex;
         for (int i = pivotIndex + 1; i <= endIndex; i++) {
             if (array[i] < array[pivotIndex]) {
