34e0cc6aa4db0ec1ca58470e2f061fcdd1a819f9
Tutku Ince <tutku.ince@outlook.com>
2024-05-27 18:37:45 +0300

    swap helper method

---
+++ src/main/java/io/tince/algorithm/sort/quick/QuickSort.java
@@ -0,0 +1,11 @@
+package io.tince.algorithm.sort.quick;
+
+public class QuickSort {
+    private static void swap(int[] array, int firstIndex, int secondIndex) {
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
+
+    
+}
