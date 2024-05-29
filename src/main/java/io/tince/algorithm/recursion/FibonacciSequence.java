92f491ae336c607de44b6bda1665b18a3756ecf8
Tutku Ince <tutku.ince@outlook.com>
2024-05-29 14:41:30 +0300

    counter

--- src/main/java/io/tince/algorithm/recursion/FibonacciSequence.java
+++ src/main/java/io/tince/algorithm/recursion/FibonacciSequence.java
@@ -1,11 +1,17 @@
 package io.tince.algorithm.recursion;
 
 public class FibonacciSequence {
+    static int counter = 0;
+
     public static void main(String[] args) {
-        System.out.println(fib(3));
+        int n = 7;
+        System.out.println("\nFib of " + n + " = " + fib(n));
+        System.out.println("\nCounter: " + counter);
+        // invoked this method 41 times without dynamic programming
     }
 
     public static int fib(int n) {
+        counter++;
         if (n == 0 || n == 1) return n;
         return fib(n - 1) + fib(n - 2);
     }
