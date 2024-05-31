274e31ef046418b9e0497310c44eb4af9c0a3050
Tutku Ince <tutku.ince@outlook.com>
2024-05-31 14:32:06 +0300

    TODO

---
+++ src/main/java/io/tince/_13array/MaxProfit.java
@@ -0,0 +1,20 @@
+package io.tince._13array;
+
+/**
+ * Array: Max Profit
+ *
+ * You are tasked with implementing a method, "maxProfit" that takes an array of integers representing the price of a given stock on different days.
+ *
+ * The method should determine the maximum profit that can be made by buying and selling the stock once. Note that you must buy before you can sell.
+ *
+ * Method Signature: public static int maxProfit(int[] prices)
+ *
+ * Input: An array of integers "prices" where each element in the array is an integer representing the price of the stock for a specific day,
+ * and where the element's index corresponds to the day number.
+ *
+ * Output: Returns an integer that is the maximum profit possible. If no profit can be made, the method should return 0.
+ *
+ * */
+public class MaxProfit {
+    // TODO
+}
