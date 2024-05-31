dd9597b14f5e21fffaf27e2070ec4cff3bd8c61c
Tutku Ince <tutku.ince@outlook.com>
2024-05-31 17:10:06 +0300

    TODO

---
+++ src/main/java/io/tince/_13array/MaxSubArray.java
@@ -0,0 +1,21 @@
+package io.tince._13array;
+
+/**
+ * Array: Max Sub Array
+ *
+ * You have been asked to create a method in Java, maxSubarray, that finds the contiguous subarray (containing at least one number)
+ * which has the largest sum and returns its sum.
+ *
+ * Method Signature: public static int maxSubarray(int[] nums)
+ *
+ * This method takes as input an array of integers "nums" and returns an integer representing the maximum of a contiguous subarray within the input array.
+ *
+ * Note
+ *  - A subarray is a contiguous part of an array. In other words, a subarray is a sequence of elements that are in direct succession in the array,
+ *  with no elements from the array skipped or interspersed.
+ *  - You need to find a contiguous subarray with the largest sum. This subarray could be the entire array,
+ *  or it could be a smaller part of the array. It could include negative numbers, but the sum of the subarray must be maximized.
+ *  - You can assume that the input array is non-empty and contains at least one number.
+ * */
+public class MaxSubArray {
+}
