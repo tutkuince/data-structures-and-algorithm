8a1128e108d2b802a39fe99bad7b2ff8a85e9ae8
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-05 17:27:54 +0300

    optimal solution

--- src/main/java/io/tince/exercises/easy/FirstBadVersion.java
+++ src/main/java/io/tince/exercises/easy/FirstBadVersion.java
@@ -28,8 +28,26 @@ public class FirstBadVersion {
         System.out.println(new FirstBadVersion().firstBadVersion(5));
     }
 
-    // Brute Force
     public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                if (mid == 1 || (mid - 1 > 0 && !isBadVersion(mid - 1))) {
+                    return mid;
+                } else {
+                    right = mid;
+                }
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    // Brute Force
+    public int firstBadVersionBruteForceApproach(int n) {
         for (int i = 1; i < n; i++) {
             if (isBadVersion(i)) return i;
         }
