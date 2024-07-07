f68a33bc398b92c91cb3ce6b5b025ac8a3c181c8
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-07 21:10:49 +0300

    explanation

---
+++ src/main/java/io/tince/exercises/easy/RepeatedSubstringPattern.java
@@ -0,0 +1,36 @@
+package io.tince.exercises.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Repeated Substring Pattern
+ *
+ * Given a string "s", check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
+ *
+ * Example 1:
+ *  Input: s = "abab"
+ *  Output: true
+ *  Explanation: It is the substring "ab" twice
+ *
+ * Example 2:
+ *  Input: s = "aba"
+ *  Output false
+ *
+ * Example 3:
+ *  Input: s = "abcabcabcabc"
+ *  Output: true
+ *  Explanation: It is the substring "abc" four times or the substring "abcabc" twice.
+ *
+ * Constraints:
+ *  - 1 <= s.length <= 10^4
+ *  - s consists of lowercase English letters.
+ * */
+public class RepeatedSubstringPattern {
+    public boolean repeatedSubstringPattern(String s) {
+        Map<String, Boolean> stringMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+
+        }
+    }
+}
