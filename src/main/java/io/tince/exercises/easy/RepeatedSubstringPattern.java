0f2b3ce701790dc98f95b47089cef2bd225690b5
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-07 21:33:14 +0300

    TODO

--- src/main/java/io/tince/exercises/easy/RepeatedSubstringPattern.java
+++ src/main/java/io/tince/exercises/easy/RepeatedSubstringPattern.java
@@ -1,7 +1,9 @@
 package io.tince.exercises.easy;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Repeated Substring Pattern
@@ -27,10 +29,21 @@ import java.util.Map;
  *  - s consists of lowercase English letters.
  * */
 public class RepeatedSubstringPattern {
+    public static void main(String[] args) {
+        System.out.println(new RepeatedSubstringPattern().repeatedSubstringPattern("abcdab"));
+    }
     public boolean repeatedSubstringPattern(String s) {
-        Map<String, Boolean> stringMap = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-
+        Set<String> uniqueStr = new HashSet<>();
+        if (s.length() == 2 && s.charAt(0) == s.charAt(1)) {
+            return true;
         }
+        for (int i = 2; i <= s.length() / 2; i++) {
+            if (!uniqueStr.contains(s.substring(0, i))) uniqueStr.add(s.substring(0, i));
+            else return true;
+        }
+        for (int i = 2; i < s.length(); i++) {
+            if (uniqueStr.contains(s.substring(i))) return true;
+        }
+        return false;
     }
 }
