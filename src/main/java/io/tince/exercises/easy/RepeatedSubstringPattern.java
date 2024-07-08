63a71421b7ab04b5a6f105bdb3f4ad1167e47c2d
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-08 14:00:52 +0300

    Repeated Substring Pattern

--- src/main/java/io/tince/exercises/easy/RepeatedSubstringPattern.java
+++ src/main/java/io/tince/exercises/easy/RepeatedSubstringPattern.java
@@ -1,49 +1,53 @@
 package io.tince.exercises.easy;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 /**
  * Repeated Substring Pattern
- *
+ * <p>
  * Given a string "s", check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
- *
+ * <p>
  * Example 1:
- *  Input: s = "abab"
- *  Output: true
- *  Explanation: It is the substring "ab" twice
- *
+ * Input: s = "abab"
+ * Output: true
+ * Explanation: It is the substring "ab" twice
+ * <p>
  * Example 2:
- *  Input: s = "aba"
- *  Output false
- *
+ * Input: s = "aba"
+ * Output false
+ * <p>
  * Example 3:
- *  Input: s = "abcabcabcabc"
- *  Output: true
- *  Explanation: It is the substring "abc" four times or the substring "abcabc" twice.
- *
+ * Input: s = "abcabcabcabc"
+ * Output: true
+ * Explanation: It is the substring "abc" four times or the substring "abcabc" twice.
+ * <p>
  * Constraints:
- *  - 1 <= s.length <= 10^4
- *  - s consists of lowercase English letters.
- * */
+ * - 1 <= s.length <= 10^4
+ * - s consists of lowercase English letters.
+ */
 public class RepeatedSubstringPattern {
     public static void main(String[] args) {
-        System.out.println(new RepeatedSubstringPattern().repeatedSubstringPattern("abcdab"));
+        System.out.println(new RepeatedSubstringPattern().repeatedSubstringPattern2("abab"));
     }
+
     public boolean repeatedSubstringPattern(String s) {
-        Set<String> uniqueStr = new HashSet<>();
-        if (s.length() == 2 && s.charAt(0) == s.charAt(1)) {
-            return true;
-        }
-        for (int i = 2; i <= s.length() / 2; i++) {
-            if (!uniqueStr.contains(s.substring(0, i))) uniqueStr.add(s.substring(0, i));
-            else return true;
-        }
-        for (int i = 2; i < s.length(); i++) {
-            if (uniqueStr.contains(s.substring(i))) return true;
+        String substr = "";
+        int n = s.length();
+
+        for (int i = 1; i <= n / 2; i++) {
+            //valid substring will have this -> (sizeOfString % sizeOfSubString == 0)
+            if (n % i == 0) {
+                substr = s.substring(0, i);
+                if (s.replaceAll(substr, "").isEmpty())
+                    return true;
+            }
         }
         return false;
+    }
+
+    public boolean repeatedSubstringPattern2(String s) {
+        String str = s + s;
+        if (str.substring(1, str.length() - 1).contains(s))
+            return true;
+        else
+            return false;
     }
 }
