da9dd909543eae3c35042534727f625858e7e350
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-14 14:09:45 +0300

    solution

---
+++ src/main/java/io/tince/exercises/easy/SymmetricTree.java
@@ -0,0 +1,44 @@
+package io.tince.exercises.easy;
+
+/**
+ * Symmetric Tree
+ * <p>
+ * Given the "root" of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+ * <p>
+ * Input: root = [1,2,2,3,4,4,3]
+ * Output: true
+ * <p>
+ * Input: root = [1,2,2,null,3,null,3]
+ * Output: false
+ * <p>
+ * Constraints:
+ * - The number of nodes in the tree is in the range [1, 1000].
+ * - -100 <= Node.val <= 100
+ */
+
+public class SymmetricTree {
+
+    public boolean isSymmetric(TreeNode root) {
+        // TODO: find the solution
+        return false;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
