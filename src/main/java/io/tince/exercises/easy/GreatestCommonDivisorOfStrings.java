ab7589d855d0ac35ea3f670c49e23454d7673a29
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-28 14:36:03 +0300

    Greatest Common Divisor of Strings

--- src/main/java/io/tince/exercises/easy/GreatestCommonDivisorOfStrings.java
+++ src/main/java/io/tince/exercises/easy/GreatestCommonDivisorOfStrings.java
@@ -1,5 +1,6 @@
 package io.tince.exercises.easy;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -21,8 +22,18 @@ import java.util.Map;
  * Output: ""
  */
 public class GreatestCommonDivisorOfStrings {
+    public static void main(String[] args) {
+        System.out.println(new GreatestCommonDivisorOfStrings().gcdOfStrings("ABABAB", "ABAB"));
+    }
+
     public String gcdOfStrings(String str1, String str2) {
-        // TODO
-        return "";
+        if (!(str1 + str2).equals(str2 + str1)) return "";
+
+        int gcd = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcd);
+    }
+
+    private int gcd(int str1Length, int str2Length) {
+        return str2Length == 0 ? str1Length : gcd(str2Length, str1Length % str2Length);
     }
 }
