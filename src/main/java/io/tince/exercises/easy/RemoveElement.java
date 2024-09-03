d8a63ba81b4d26d429d6f80820c90f0e0e5d483b
TUTKU\tutku <tutku.ince@outlook.com>
2024-09-03 15:09:28 +0300

    solution 2

--- src/main/java/io/tince/exercises/easy/RemoveElement.java
+++ src/main/java/io/tince/exercises/easy/RemoveElement.java
@@ -51,4 +51,15 @@ public class RemoveElement {
         }
         return result;
     }
+
+    public int removeElement2(int[] nums, int val) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
 }
