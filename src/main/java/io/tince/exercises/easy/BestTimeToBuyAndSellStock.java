dec319efb5dbdd3220a5e3b505ce3964dfdfad27
Tutku Ince <tutku.ince@outlook.com>
2024-09-11 14:26:26 +0300

    Best Time to Buy and Sell Stock Solution

--- src/main/java/io/tince/exercises/easy/BestTimeToBuyAndSellStock.java
+++ src/main/java/io/tince/exercises/easy/BestTimeToBuyAndSellStock.java
@@ -7,21 +7,38 @@ package io.tince.exercises.easy;
  * <p>
  * You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
  * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return "0".
- *
+ * <p>
  * Example 1:
  * Input: prices = [7,1,5,3,6,4]
  * Output: 5
  * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
  * Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
- *
+ * <p>
  * Example 2:
  * Input: prices = [7,6,4,3,1]
  * Output: 0
  * Explanation: In this case, no transactions are done and the max profit = 0.
  */
 public class BestTimeToBuyAndSellStock {
-    public int maxProfit(int[] prices) {
-        // TODO - Find the solution
-        return 0;
+    public static void main(String[] args) {
+        int[] prices1 = new int[]{7, 1, 5, 3, 6, 4};
+        int[] prices2 = new int[]{7, 6, 4, 3, 1};
+        System.out.println(maxProfit(prices2));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++) {
+            if (min > prices[i]) {
+                min = prices[i];
+            }
+            if (min < prices[i]) {
+                if (maxProfit < prices[i] - min) {
+                    maxProfit = prices[i] - min;
+                }
+            }
+        }
+        return maxProfit;
     }
 }
