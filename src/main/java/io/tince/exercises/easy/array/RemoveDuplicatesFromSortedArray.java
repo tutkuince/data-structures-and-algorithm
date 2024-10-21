eef4cd680b766a8acc3d2b5e131524d6ddee19fd
Tutku Ince <tutku.ince@outlook.com>
2024-10-21 15:47:39 +0300

    Remove Duplicates From Sorted Array

---
+++ src/main/java/io/tince/exercises/easy/array/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,48 @@
+package io.tince.exercises.easy.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Given an integer array "nums" sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once.
+ * The relative order of the elements should be kept the same. The return the number of unique elements in "nums".
+ * <p>
+ * Consider the number of unique elements of "nums" to be "k", to get accepted, you need to do the following things:
+ * - Change the array "nums" such that the first "k" elements of "nums" contain the unique elements in the order they were present in "nums"
+ * initially. The remaining elements of "nums" are not important as well as the size of "nums".
+ * - Return k.
+ * <p>
+ * Example 1:
+ * Input: nums = [1, 1, 2]
+ * Output: 2, nums = [1, 2, _]
+ * Explanation: Your function should return k = 2, with the first two elements of nums being 1 and respectively.
+ * It does not matter what you leave beyond the returned k (hence they are underscores).
+ * <p>
+ * Example 2:
+ * Input: nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+ * Output: 5, nums = [0, 1, 2, 3, 4, _, _, _, _, _]
+ * Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3 and 4.
+ * It does not matter what you leave beyond the returned k (hence they are underscores).
+ */
+public class RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+        int[] numbers1 = {1, 1, 2};                         // [1, 2, 2]
+        int[] numbers2 = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};    // [0, 1, 2, 3, 4, 2, 2, 3, 3, 4]
+        System.out.println(removeDuplicates(numbers1));
+    }
+
+    // Solution - 1
+    public static int removeDuplicates(int[] nums) {
+        List<Integer> uniqueNumbers = new ArrayList<>();
+        for (int i : nums) {
+            if (!uniqueNumbers.contains(i)) {
+                uniqueNumbers.add(i);
+            }
+        }
+        for (int i = 0; i < uniqueNumbers.size(); i++) {
+            nums[i] = uniqueNumbers.get(i);
+        }
+        return uniqueNumbers.size();
+    }
+}
