ebe21394ee31aae70cec9bc032257df097093e59
Tutku Ince <tutku.ince@outlook.com>
2024-10-21 19:39:55 +0300

    Rotate Array

---
+++ src/main/java/io/tince/exercises/easy/array/RotateArray.java
@@ -0,0 +1,50 @@
+package io.tince.exercises.easy.array;
+
+import java.util.Arrays;
+
+/**
+ * Rotate Array
+ * <p>
+ * Given an integer array "nums", rotate the array to the right by "k" steps, where "k" is non-negative.
+ * <p>
+ * Example 1:
+ * Input: nums = [1,2,3,4,5,6,7], k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ * <p>
+ * Example 2:
+ * Input: nums = [-1,-100,3,99], k = 2
+ * Output: [3,99,-1,-100]
+ * Explanation:
+ * rotate 1 steps to the right: [99,-1,-100,3]
+ * rotate 2 steps to the right: [3,99,-1,-100]
+ */
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 4, 5, 6, 7};
+        int nums1K = 3;
+        rotate(nums1, nums1K);
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    // 1, 2, 3
+    public static void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
