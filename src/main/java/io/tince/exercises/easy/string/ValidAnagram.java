16b5e4e919a96542cc9a934699fbdd32f1a7de57
Tutku Ince <tutku.ince@outlook.com>
2024-10-25 14:38:20 +0300

    Valid Anagram

---
+++ src/main/java/io/tince/exercises/easy/string/ValidAnagram.java
@@ -0,0 +1,32 @@
+package io.tince.exercises.easy.string;
+
+import java.util.Arrays;
+
+/**
+ * Valid Anagram
+ * <p>
+ * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+ * <p>
+ * Example 1:
+ * Input: s = "anagram", t = "nagaram"
+ * Output: true
+ * <p>
+ * Example 2:
+ * Input: s = "rat", t = "car"
+ * Output: false
+ */
+public class ValidAnagram {
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram"));
+        System.out.println(isAnagram("rat", "car"));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        char[] sCharArray = s.toCharArray();
+        char[] tCharArray = t.toCharArray();
+        Arrays.sort(sCharArray);
+        Arrays.sort(tCharArray);
+        return Arrays.equals(sCharArray, tCharArray);
+
+    }
+}
