e72c85895dd9a92d7a2a12e49ba6ffef05ae2b2a
Tutku Ince <tutku.ince@outlook.com>
2024-12-19 18:45:15 +0300

    Running Sum of 1D Array

---
+++ src/main/java/io/tince/exercises/easy/RunningSumOfArray.java
@@ -0,0 +1,41 @@
+package io.tince.exercises.easy;
+
+import java.util.Arrays;
+
+/**
+ * Running Sum of 1D Array
+ * <p>
+ * Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+ * <p>
+ * Return the running sum of nums.
+ * <p>
+ * Example 1:
+ * Input: nums = [1,2,3,4]
+ * Output: [1,3,6,10]
+ * Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+ * <p>
+ * Example 2:
+ * Input: nums = [1,1,1,1,1]
+ * Output: [1,2,3,4,5]
+ * Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+ * <p>
+ * Example 3:
+ * Input: nums = [3,1,2,10,1]
+ * Output: [3,4,6,16,17]
+ */
+public class RunningSumOfArray {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 4};
+        int[] nums2 = {1, 1, 1, 1, 1};
+        int[] nums3 = {3, 1, 2, 10, 1};
+
+        System.out.println(Arrays.toString(runningSum(nums3)));
+    }
+
+    public static int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+}
