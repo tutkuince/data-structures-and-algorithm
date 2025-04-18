d9671466034aebc5689543a067955e997cb533db
Tutku Ince <tutku.ince@outlook.com>
2025-04-18 13:56:04 +0300

    problem

---
+++ src/main/java/io/tince/exercises/easy/string/CommonCharacters.java
@@ -0,0 +1,19 @@
+package io.tince.exercises.easy.string;
+
+/**
+ * Common Characters
+ *
+ * Write a function that takes in a non-empty list of non-empty strings and returns a list of characters that are common to all strings in the list,
+ * ignoring multiplicity.
+ *
+ * Note that the strings are not guaranteed to only contain alphanumeric characters. The list you return can be in any order.
+ *
+ * Sample Input: strings = ["abc", "bcd", "cbaccd"]
+ * Sample Output: ["b", "c"]    // the characters could be ordered differently.
+ * */
+public class CommonCharacters {
+    public String[] commonCharacters(String[] strings) {
+        // Write your code here.
+        return new String[] {};
+    }
+}
