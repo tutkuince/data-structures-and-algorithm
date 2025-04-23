c1cb3035c3f618c33e5525fee71677a49a865169
Tutku Ince <tutku.ince@outlook.com>
2025-04-23 14:47:47 +0300

    problem

---
+++ src/main/java/io/tince/exercises/easy/string/FirstNonRepeatingCharacter.java
@@ -0,0 +1,25 @@
+package io.tince.exercises.easy.string;
+
+import java.util.*;
+
+/**
+ * First Non-Repeating Character
+ * <p>
+ * Write a function that takes in a string of lowercase English-alphabet letters and returns the index of the string's first non-repeating character.
+ * <p>
+ * The first non-repeating character is the first character in a string that occurs only once.
+ * If the input string does not have any non-repeating characters, your function should return -1.
+ * <p>
+ * Sample Input: "abcdcaf"
+ * Sample Output: 1 -> The first non-repeating character is "b" and is found at index 1.
+ */
+public class FirstNonRepeatingCharacter {
+    public static void main(String[] args) {
+        System.out.println(firstNonRepeatingCharacter("abcdcaf"));
+    }
+
+    public static int firstNonRepeatingCharacter(String string) {
+        // Write your code here.
+        return -1;
+    }
+}
