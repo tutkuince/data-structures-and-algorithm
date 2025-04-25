73ec2c89abff4272068c90a087e1c03e5d9bca3f
Tutku Ince <tutku.ince@outlook.com>
2025-04-25 15:23:38 +0300

    problem

---
+++ src/main/java/io/tince/exercises/easy/string/Semordnilap.java
@@ -0,0 +1,23 @@
+package io.tince.exercises.easy.string;
+
+import java.util.List;
+
+/**
+ * Semordnilap
+ * <p>
+ * Write a function that takes in a list of uniques strings and returns a list of semordnilap pairs.
+ * <p>
+ * A semordnilap pair is defined as a set of different strings where the reverse of one word is the same as the forward version of the order.
+ * For example the words "diaper" and "repaid" are a semordnilap pair, as are the words "palindromes" and "semordnilap".
+ * <p>
+ * The order of the returned pairs and the order of the strings within each pair does not matter.
+ * <p>
+ * Sample Input: words = ["diaper", "abc", "test", "cba", "repaid"]
+ * Sample Output: [["diaper", "repaid"], ["abc", "cba"]]
+ */
+public class Semordnilap {
+    public List<List<String>> semordnilap(String[] words) {
+        // Write your code here.
+        return null;
+    }
+}
