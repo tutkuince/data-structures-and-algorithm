22fc482bd283e604ff792477fd6f3fb42bd0e8da
Tutku Ince <tutku.ince@outlook.com>
2025-04-25 16:04:07 +0300

    solutions

--- src/main/java/io/tince/exercises/easy/string/Semordnilap.java
+++ src/main/java/io/tince/exercises/easy/string/Semordnilap.java
@@ -1,6 +1,9 @@
 package io.tince.exercises.easy.string;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Semordnilap
@@ -16,8 +19,42 @@ import java.util.List;
  * Sample Output: [["diaper", "repaid"], ["abc", "cba"]]
  */
 public class Semordnilap {
-    public List<List<String>> semordnilap(String[] words) {
+    public static void main(String[] args) {
+        String[] words1 = {"abcdefghijk", "kjihgfedcba"};
+        String[] words2 = {"diaper", "abc", "test", "cba", "repaid"};
+        System.out.println(semordnilap2(words2));
+    }
+
+    public static List<List<String>> semordnilap1(String[] words) {
         // Write your code here.
-        return null;
+        List<List<String>> result = new ArrayList<>();
+        for (int i = 0; i < words.length - 1; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                StringBuilder builder = new StringBuilder(words[i]);
+                String reveresWord = builder.reverse().toString();
+                if (words[j].equals(reveresWord)) {
+                    result.add(List.of(words[i], reveresWord));
+                }
+            }
+        }
+        return result;
+    }
+
+    public static List<List<String>> semordnilap2(String[] words) {
+        // Write your code here.
+        List<List<String>> result = new ArrayList<>();
+        Set<String> wordSet = new HashSet<>();
+        for (String word : words) {
+            StringBuilder builder = new StringBuilder(word);
+            String reversedWord = builder.reverse().toString();
+            if (wordSet.contains(reversedWord)) {
+                ArrayList<String> list = new ArrayList<>();
+                list.add(word);
+                list.add(reversedWord);
+                result.add(list);
+            }
+            wordSet.add(word);
+        }
+        return result;
     }
 }
