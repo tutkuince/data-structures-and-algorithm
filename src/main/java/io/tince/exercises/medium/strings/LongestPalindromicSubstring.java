9960271d501d76b32928895cf72e5214abaaf41b
Tutku Ince <tutku.ince@outlook.com>
2025-06-12 18:49:33 +0300

    Longest Palindromic Substring

---
+++ src/main/java/io/tince/exercises/medium/strings/LongestPalindromicSubstring.java
@@ -0,0 +1,18 @@
+package io.tince.exercises.medium.strings;
+
+/**
+ * Longest Palindromic Substring
+ * Write a function that, given a string, returns its longest palindromic substring.
+ * A palindrome is defined as a string that's written the same forward and backward. Note that single-character strings are palindromes.
+ * You can assume that there will only be one longest palindromic substring.
+ *
+ * Sample Input: string = "abaxyzzyxf"
+ * Sample Output: "xyzzyx"
+ * */
+public class LongestPalindromicSubstring {
+    public static String longestPalindromicSubstring(String str) {
+        // Write your code here.
+        // TODO
+        return "";
+    }
+}
