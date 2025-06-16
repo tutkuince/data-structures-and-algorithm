12784bc3f5c33df421595e70c12b1d423868cf5e
Tutku Ince <tutku.ince@outlook.com>
2025-06-17 00:03:26 +0300

    TODO

---
+++ src/main/java/io/tince/exercises/medium/strings/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,36 @@
+package io.tince.exercises.medium.strings;
+
+/**
+ * Longest Substring Without Repeating Characters
+ * <p>
+ * Given a string s, find the length of the longest substring without duplicate characters.
+ * <p>
+ * Example 1:
+ * Input: s = "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ * <p>
+ * Example 2:
+ * Input: s = "bbbbb"
+ * Output: 1
+ * Explanation: The answer is "b", with the length of 1.
+ * <p>
+ * Example 3:
+ * Input: s = "pwwkew"
+ * Output: 3
+ * Explanation: The answer is "wke", with the length of 3.
+ * Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring("bbbbb"));
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        
+        return 0;
+    }
+}
