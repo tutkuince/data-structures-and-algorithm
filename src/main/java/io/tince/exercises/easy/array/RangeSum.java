b799e8c650297d233c08a88f6e7a8eeaf567d685
Tutku Ince <tutku.ince@outlook.com>
2025-06-25 14:41:55 +0300

    range sum solution

--- src/main/java/io/tince/exercises/easy/array/RangeSum.java
+++ src/main/java/io/tince/exercises/easy/array/RangeSum.java
@@ -1,26 +1,20 @@
 package io.tince.exercises.easy.array;
 
 public class RangeSum {
-    private int[] prefixSum;
-
     public static void main(String[] args) {
-        int[] nums = {3, 2, -1, 6, 5, 4, -3, 3};
-        RangeSum rangeSum = new RangeSum(nums);
-        System.out.println(rangeSum.sumRange(0, 2));
-        System.out.println(rangeSum.sumRange(3, 5));
-        System.out.println(rangeSum.sumRange(0, 7));
-        System.out.println(rangeSum.sumRange(2, 6));
+        int[] nums = {2, 4, 1, 3, 6, 5};
+        System.out.println(sumRange(1, 3, nums));
+        System.out.println(sumRange(0, 2, nums));
+        System.out.println(sumRange(2, 5, nums));
     }
 
-    public RangeSum(int[] nums) {
-        prefixSum = new int[nums.length + 1];
+    public static int sumRange(int startIndex, int endIndex, int[] nums) {
+        int[] prefix = new int[nums.length + 1];
 
         for (int i = 0; i < nums.length; i++) {
-            prefixSum[i + 1] = prefixSum[i] + nums[i];
+            prefix[i + 1] = nums[i] + prefix[i];
         }
-    }
 
-    public int sumRange(int startIndex, int endIndex) {
-        return prefixSum[endIndex + 1] - prefixSum[startIndex];
+        return prefix[endIndex + 1] - prefix[startIndex];
     }
 }
